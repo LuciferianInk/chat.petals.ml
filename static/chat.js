ec8523e04160a16f3437c28edc3587498da85616
Luciferian Ink <LuciferianInk@protonmail.com>
2023-05-24 18:03:19 -0500

    fix params

--- static/chat.js
+++ static/chat.js
@@ -26,7 +26,13 @@ var curModel = default_model_name;
 const generationParams = {
   do_sample: 1,
   temperature: 0.9,
-  top_k: 40,
+  top_k: 4,
+  penalty_alpha: 0.6,
+  repetition_penalty: 2.3,
+  encoder_repetition_penalty: 2.3,
+  no_repeat_ngram_size: 4,
+  encoder_no_repeat_ngram_size: 4,
+  renormalize_logits: true
 };
 
 var ws = null;
